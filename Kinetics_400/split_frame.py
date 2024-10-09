0df6ae888064dc38d7f99b5d17f2d33f5410ff8f
liujf69 <1336875738@qq.com>
2024-10-09 17:39:31 +0800

    first commit

--- Kinetics_400/split_frame.py
+++ Kinetics_400/split_frame.py
@@ -2,13 +2,20 @@ import os
 import csv
 import cv2
 
+def find_video_name(root_video_path, label, youtube_id):
+    video_path = os.path.join(root_video_path, label)
+    video_names = os.listdir(video_path)
+    for idx, video_name in enumerate(video_names):
+        if(video_name[:11] == youtube_id):
+            return video_name
+
 def split_frame(raw_csv: str, root_video_path: str, root_output_path: str, debug: bool = True):
     csv_reader = csv.reader(open(raw_csv))
     for idx, row in enumerate(csv_reader): 
         if (idx == 0):
             continue # ['label', 'youtube_id', 'time_start', 'time_end', 'split', 'is_cc'] 
         label, youtube_id, time_start, time_end, split, is_cc = row
-        video_name = youtube_id + "_" + time_start.zfill(6) + "_" + time_end.zfill(6) + ".mp4"
+        video_name = find_video_name(root_video_path, label, youtube_id)
         print("Process ", idx, " ", video_name)
         
         video_path = os.path.join(root_video_path, label, video_name)
@@ -31,7 +38,7 @@ def split_frame(raw_csv: str, root_video_path: str, root_output_path: str, debug
 if __name__ == "__main__":
     raw_csv = "./label/train_256.csv"
     root_video_path = "./raw-part/compress/train_256"
-    root_output_path = "./output"
+    root_output_path = "./test"
     split_frame(raw_csv = raw_csv, root_video_path = root_video_path, root_output_path = root_output_path, debug = True)
 
     print("All Done!")
